723e98c95f4db33b9ee9ec79f41e4546f6558902
doxxx <doxxx93@gmail.com>
2022-08-04 22:52:08 +0900

    init commit

---
+++ bliond/src/main/java/com/likelion/bliond/BliondApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.bliond;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BliondApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BliondApplication.class, args);
+    }
+
+}
