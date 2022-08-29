899417dc4f70903a85485679fabf255885fc47fc
heokyeongju <beewt@naver.com>
2022-08-29 23:47:57 +0900

    🍀 feat: User 도메인 구현

--- bliond/src/main/java/com/likelion/bliond/domain/User.java
+++ bliond/src/main/java/com/likelion/bliond/domain/User.java
@@ -1,4 +1,45 @@
 package com.likelion.bliond.domain;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class User {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Status status;
+
+    @Column(nullable = false)
+    private String password;
+
+
+    @Builder
+    public User(Long id, String password, String name, String email, Role role, Status status) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+        this.status = status;
+        this.name = name;
+    }
+
 }
