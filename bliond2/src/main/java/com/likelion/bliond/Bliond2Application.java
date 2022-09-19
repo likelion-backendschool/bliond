99456bb88d989420b3360bcf96e6a1f3dfb7eb17
doxxx <doxxx93@gmail.com>
2022-09-20 00:54:22 +0900

    setting

---
+++ bliond2/src/main/java/com/likelion/bliond/Bliond2Application.java
@@ -0,0 +1,13 @@
+package com.likelion.bliond;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bliond2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bliond2Application.class, args);
+    }
+
+}
---
+++ bliond2/src/test/java/com/likelion/bliond/Bliond2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.bliond;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bliond2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
